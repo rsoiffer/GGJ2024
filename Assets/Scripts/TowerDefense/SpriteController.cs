bf65c97df610c315808618d3423ad804b4b5ef2f
rsoiffer <rsoiffer@gmail.com>
2024-01-27 18:29:09 -0800

    Implemented walk animation

--- Assets/Scripts/TowerDefense/SpriteController.cs
+++ Assets/Scripts/TowerDefense/SpriteController.cs
@@ -1,5 +1,6 @@
 ﻿using Data;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace TowerDefense
 {
@@ -11,16 +12,39 @@ namespace TowerDefense
         public SpriteRenderer iconSprite;
         public SpriteRenderer selectionRing;
 
+        public Vector2 lastDirection;
+        public float walkAnimSpeed = 4f;
+        public float walkAnimSpeedNoise = .1f;
+        public bool isWalking;
+
+        private float _walkAnimSpeedActual;
+
         public PokemonSpriteSet SpriteSet => pokemon.isShiny ? pokemon.data.spriteSetShiny : pokemon.data.spriteSet;
+
+        private void Awake()
+        {
+            _walkAnimSpeedActual = walkAnimSpeed + walkAnimSpeedNoise * Random.Range(-1, 1);
+        }
+
+        private void LateUpdate()
+        {
+            if (followerSprite.enabled)
+            {
+                var spriteId = 0;
+                var max = Mathf.Max(Mathf.Abs(lastDirection.x), Mathf.Abs(lastDirection.y));
+                if (lastDirection.y >= max) spriteId = 0;
+                if (lastDirection.x >= max) spriteId = 4;
+                if (-lastDirection.x >= max) spriteId = 8;
+                if (-lastDirection.y >= max) spriteId = 12;
+
+                if (isWalking) spriteId += Mathf.FloorToInt(Time.time * _walkAnimSpeedActual) % 4;
+                followerSprite.sprite = SpriteSet.followers[spriteId];
+            }
+        }
 
         public void Look(Vector2 direction)
         {
-            var max = Mathf.Max(Mathf.Abs(direction.x), Mathf.Abs(direction.y));
-            if (direction.y >= max) followerSprite.sprite = SpriteSet.followers[0];
-            if (direction.x >= max) followerSprite.sprite = SpriteSet.followers[4];
-            if (-direction.x >= max) followerSprite.sprite = SpriteSet.followers[8];
-            if (-direction.y >= max) followerSprite.sprite = SpriteSet.followers[12];
-
+            lastDirection = direction;
             followerSprite.enabled = true;
             shadow.enabled = true;
             iconSprite.enabled = false;
