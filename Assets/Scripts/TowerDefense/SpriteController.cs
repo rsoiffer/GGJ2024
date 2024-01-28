562a5cd286222e16a76b5a226e1d51a6f3a1622f
rsoiffer <rsoiffer@gmail.com>
2024-01-27 18:22:00 -0800

    Added shadows and sprite offset

--- Assets/Scripts/TowerDefense/SpriteController.cs
+++ Assets/Scripts/TowerDefense/SpriteController.cs
@@ -6,7 +6,9 @@ namespace TowerDefense
     public class SpriteController : MonoBehaviour
     {
         public PokemonInstance pokemon;
-        public SpriteRenderer sprite;
+        public SpriteRenderer followerSprite;
+        public SpriteRenderer shadow;
+        public SpriteRenderer iconSprite;
         public SpriteRenderer selectionRing;
 
         public PokemonSpriteSet SpriteSet => pokemon.isShiny ? pokemon.data.spriteSetShiny : pokemon.data.spriteSet;
@@ -14,15 +16,23 @@ namespace TowerDefense
         public void Look(Vector2 direction)
         {
             var max = Mathf.Max(Mathf.Abs(direction.x), Mathf.Abs(direction.y));
-            if (direction.y >= max) sprite.sprite = SpriteSet.followers[0];
-            if (direction.x >= max) sprite.sprite = SpriteSet.followers[4];
-            if (-direction.x >= max) sprite.sprite = SpriteSet.followers[8];
-            if (-direction.y >= max) sprite.sprite = SpriteSet.followers[12];
+            if (direction.y >= max) followerSprite.sprite = SpriteSet.followers[0];
+            if (direction.x >= max) followerSprite.sprite = SpriteSet.followers[4];
+            if (-direction.x >= max) followerSprite.sprite = SpriteSet.followers[8];
+            if (-direction.y >= max) followerSprite.sprite = SpriteSet.followers[12];
+
+            followerSprite.enabled = true;
+            shadow.enabled = true;
+            iconSprite.enabled = false;
         }
 
         public void SetToIcon()
         {
-            sprite.sprite = SpriteSet.icons[0];
+            iconSprite.sprite = SpriteSet.icons[0];
+
+            followerSprite.enabled = false;
+            shadow.enabled = false;
+            iconSprite.enabled = true;
         }
     }
 }
